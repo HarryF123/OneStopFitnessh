0a2bcef27cae4a55d2738575ee22243ffad58143
Harry Freney <H.FRENEY1@nuigalway.ie>
2021-04-18 14:25:26 +0100

    Touching up titles, icons

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,5 +1,5 @@
 /*!
-    * Start Bootstrap - Arm Exercises - OneStop Fitnesscer v6.0.5 (https://startbootstrap.com/theme/freelancer)
+    * Start Bootstrap - Freelancer v6.0.5 (https://startbootstrap.com/theme/freelancer)
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
     */
