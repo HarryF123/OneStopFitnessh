88c434a5879c71a763d76b99621dc1716451e86c
Harry Freney <H.FRENEY1@nuigalway.ie>
2021-04-18 18:14:29 +0100

    trying changes

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,6 @@ const admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
 
 admin.initializeApp();
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
